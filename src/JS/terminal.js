7198b8845fc0856fb5ed422cf5bdad85f9b28e5d
salarini-e <eduardo.pmnf@gmail.com>
2021-10-16 15:29:53 -0300

    terminal 0.0.2

--- src/JS/terminal.js
+++ src/JS/terminal.js
@@ -1,19 +1,37 @@
 inputs_term = document.getElementsByClassName("iterm");
 
-list_t=['t1','t2','t3','t4', 'stop']
-list_r=['r1','r2','r3','r4', 'stop']
+list_t=['t1','t2','t3','t4', 't5']
+list_r=['r1','r2','r3','r4', 'r4']
 
 count_input=0
+loop=false
 $("#terminal-form").submit(function(){
-    if(list_t[count_input]=='stop'){
+    if(list_t[count_input]=='t5'){
+       if(loop==false){
         count_input=0
-        return false;
+        change=list_t
+        list_t=list_r
+        list_r=change
+        document.getElementById('terminal-form').reset()
+        loop=true
+       }
     }
     $('#'+list_t[count_input]).prop('disabled', true); 
     document.getElementById(list_r[count_input]).value='teste';
-    count_input=count_input+1;
-    $('#'+list_t[count_input]).prop('disabled', false);
-    document.getElementById(list_t[count_input]).focus(); 
+    if(list_t[count_input]=='t5'){
+        count_input=-1
+        // return false;
+    }
+    if (loop==true){
+        count_input=count_input;
+        $('#'+list_t[count_input]).prop('disabled', false);
+        document.getElementById(list_t[count_input]).focus(); 
+    }else{
+        count_input=count_input+1;
+        $('#'+list_t[count_input]).prop('disabled', false);
+        document.getElementById(list_t[count_input]).focus(); 
+    }
+    loop=false
     return false;
 });
   
